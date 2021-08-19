4ff92a5a96e26c20747bf44c4da424cd3797b33f
sinenhlanhla magubane <snemagubane06@gmail.com>
2021-08-19 15:15:49 +0200

    level 0 commit

--- task06.js
+++ task06.js
@@ -1,11 +1,13 @@
 function maxNumber( a,b,c) {
-    if ( a > b && a > c) {
+    if (( a > b && a > c) || (c===b && a > b) || ( a ===b && a > c)){
         return a;
-    } else if ( b > a && b > c) {
+    } else if (( b > a && b > c) || (c === a && b> c) || (b ===c && b > a)) {
         return  b;
-        } else {
-            return  c;
-        } 
+    }else if (( c > a && c > b) || ( a ===b && c > a) || (a ===c && c > b)){
+        return c;
+    }else{
+        return c;
+    } 
      
 }
-console.log(maxNumber(1,4,6));
+console.log(maxNumber(1,-6,1));
